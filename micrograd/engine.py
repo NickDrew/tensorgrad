03b284e051dfa20b262ef520c50bbaef72eb745d
NickDrew <nicholasadrew@hotmail.com>
2023-05-01 13:37:42 +0100

    convert value init to array

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,14 +1,17 @@
 
+
 class Value:
-    """ stores a single scalar value and its gradient """
+    """ stores an array of scalar values and their gradient """
 
     def __init__(self, data, _children=(), _op=''):
         self.data = data
-        self.grad = 0
+        self.grad = []
+        for dp in data:
+            self.grad.append(0)
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._op = _op  # the op that produced this node, for graphviz / debugging / etc
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -33,7 +36,8 @@ class Value:
         return out
 
     def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        assert isinstance(other, (int, float)
+                          ), "only supporting int/float powers for now"
         out = Value(self.data**other, (self,), f'**{other}')
 
         def _backward():
@@ -56,6 +60,7 @@ class Value:
         # topological order all of the children in the graph
         topo = []
         visited = set()
+
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
@@ -69,25 +74,25 @@ class Value:
         for v in reversed(topo):
             v._backward()
 
-    def __neg__(self): # -self
+    def __neg__(self):  # -self
         return self * -1
 
-    def __radd__(self, other): # other + self
+    def __radd__(self, other):  # other + self
         return self + other
 
-    def __sub__(self, other): # self - other
+    def __sub__(self, other):  # self - other
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other):  # other - self
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other):  # other * self
         return self * other
 
-    def __truediv__(self, other): # self / other
+    def __truediv__(self, other):  # self / other
         return self * other**-1
 
-    def __rtruediv__(self, other): # other / self
+    def __rtruediv__(self, other):  # other / self
         return other * self**-1
 
     def __repr__(self):
